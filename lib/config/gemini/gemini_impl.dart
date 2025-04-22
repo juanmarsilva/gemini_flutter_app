b7fdbc1b0d2b4f72bfc7577b4d1c1d8a76439fdf
Juan Martin Silva <juanmartinsilva74@gmail.com>
2025-04-22 00:57:34 -0300

    md create call to service

---
+++ lib/config/gemini/gemini_impl.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class GeminiImpl {
+  final Dio _http = Dio(BaseOptions(baseUrl: dotenv.env['ENDPOINT_API'] ?? ''));
+
+  Future<String> getResponse(String prompt) async {
+    try {
+      final body = jsonEncode({'prompt': prompt});
+
+      final response = await _http.post('/basic-prompt', data: body);
+
+      return response.data;
+    } catch (e) {
+      throw Exception("Can't get Gemini response");
+    }
+  }
+}
