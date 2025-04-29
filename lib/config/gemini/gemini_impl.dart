f3525b6dd709e76fb51db7f47fb324f5edb51074
Juan Martin Silva <juanmartinsilva74@gmail.com>
2025-04-28 23:29:13 -0300

    md add get response stream method

--- lib/config/gemini/gemini_impl.dart
+++ lib/config/gemini/gemini_impl.dart
@@ -18,4 +18,30 @@ class GeminiImpl {
       throw Exception("Can't get Gemini response");
     }
   }
+
+  Stream<String> getResponseStream(String prompt) async* {
+    try {
+      final body = jsonEncode({'prompt': prompt});
+
+      final response = await _http.post(
+        '/basic-prompt-stream',
+        data: body,
+        options: Options(responseType: ResponseType.stream),
+      );
+
+      final stream = response.data.stream as Stream<List<int>>;
+
+      String buffer = '';
+
+      await for (final chunk in stream) {
+        final chunkString = utf8.decode(chunk, allowMalformed: true);
+
+        buffer += chunkString;
+
+        yield buffer;
+      }
+    } catch (e) {
+      throw Exception("Can't get Gemini response");
+    }
+  }
 }
