c7c3c85e7e9151d851e433eccc2f1b8463f50252
Juan Martin Silva <juanmartinsilva74@gmail.com>
2025-05-11 18:33:06 -0300

    md create new provider

---
+++ lib/presentation/providers/image/generated_images_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'generated_images_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$generatedImagesHash() => r'ad90867ac91ffd215eed52c0b3debcf40e81aeeb';
+
+/// See also [GeneratedImages].
+@ProviderFor(GeneratedImages)
+final generatedImagesProvider =
+    AutoDisposeNotifierProvider<GeneratedImages, List<String>>.internal(
+      GeneratedImages.new,
+      name: r'generatedImagesProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$generatedImagesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$GeneratedImages = AutoDisposeNotifier<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
